83ada103ba6a2f05a0ae6098240a31f1f354d721
meeheey <m.m.zeon@gmail.com>
2025-03-17 15:23:42 +0100

    Change icons, fix searchbar CSS

--- bible_in_serbian/web_page/urls.py
+++ bible_in_serbian/web_page/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path('books/<int:book_id>/', views.show_book, name='show_book'),
-    path('comparative_reading/', views.comparative_reading, name='comparative reading'),
+    path('comparative_reading/', views.comparative_reading, name='comparative reading')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
